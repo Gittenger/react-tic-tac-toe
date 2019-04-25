3d16bdb1919d5b0d144eac7db82a61f7f3e11699
Gittenger <john.pittenger86@gmail.com>
2019-04-24 22:02:27 -0400

    alternating x and o working

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
+//function component
 function Square(props) {
   return (
-    <button className="square" onClick={props.onClick()}>
+    <button className="square" onClick={props.onClick}>
       {props.value}
     </button>
   );
@@ -13,15 +14,17 @@ function Square(props) {
 class Board extends React.Component {
   constructor(props) {
     super(props);
+    //initial state of board
     this.state = {
-      squares: Array(9).fill(null)
+      squares: Array(9).fill(null),
+      xIsNext: true
     };
   }
 
   handleClick(i) {
     const squares = this.state.squares.slice();
-    squares[i] = "X";
-    this.setState({ squares: squares });
+    squares[i] = this.state.xIsNext ? "X" : "O";
+    this.setState({ squares: squares, xIsNext: !this.state.xIsNext });
   }
 
   renderSquare(i) {
